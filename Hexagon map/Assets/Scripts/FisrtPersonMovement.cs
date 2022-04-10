ae445d318b10ae0fef13119304c7ed1a628bbc43
gm398 <gm398@sussex.ac.uk>
2022-04-10 23:56:24 +0100

    added base first person character

---
+++ Hexagon map/Assets/Scripts/FisrtPersonMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FisrtPersonMovement : MonoBehaviour
+{
+
+    [SerializeField] CharacterController controller;
+    [SerializeField] float speed = 10;
+    
+    bool isGrounded;
+    
+    Vector3 velocity;
+    float hInput, vInput;
+
+    bool isJumping;
+    [SerializeField] float jumpHeight = 3f;
+    [SerializeField] float jumpGravity = -20;
+    [SerializeField] float fallGravity = -40;
+    float gravity = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = controller.isGrounded;
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        isJumping = Input.GetButton("Jump");
+
+        Gravity();
+        GroundMovement();
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+    
+    public void GroundMovement()
+    {
+        
+        Vector3 move = transform.right * hInput + transform.forward * vInput;
+        velocity.x = move.x * speed;
+        velocity.z = move.z * speed;
+
+        if(velocity.y > 0) { gravity = jumpGravity; }
+        else { gravity = fallGravity; }
+        if(isJumping && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * jumpGravity);
+        }
+            
+    }
+
+    public void Gravity()
+    {
+        velocity.y += gravity * Time.deltaTime;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+    }
+
+}
---
+++ Hexagon map/Assets/Scripts/mouseLook.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+    private float xRotation = 0f;
+    [SerializeField] string camZoom = "t";
+    [SerializeField] Camera cam;
+    [SerializeField] Transform CamAnchor;
+    [SerializeField] Transform ThirdPersonAnchor;
+    private bool isFP = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        isFP = true;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        CamAnchor.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+        if(Input.GetKey(camZoom))
+        {
+            cam.fieldOfView = 45;
+        }
+        else { cam.fieldOfView = 60; }
+        if (Input.GetKeyDown("tab"))
+        {
+            SwitchCams();
+        }
+    }
+
+    private void SwitchCams()
+    {
+        if (isFP)
+        {
+            transform.position = ThirdPersonAnchor.position;
+        }
+        else
+        {
+            transform.position = CamAnchor.position;
+        }
+        isFP = !isFP;
+    }
+}
